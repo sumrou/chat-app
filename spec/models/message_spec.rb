413214581e6ef36eb6ce42d67e888a34ad9d31d8
sumrou <69175299+sumrou@users.noreply.github.com>
2020-09-10 00:06:44 +0900

    結合テストの実装

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -9,21 +9,35 @@ RSpec.describe Message, type: :model do
 
     it 'contentとimageが存在していれば保存できること' do
       expect(@message).to be_valid
-  
     end
+
     it 'contentが存在していれば保存できること' do
       @message.image = nil
       expect(@message).to be_valid
     end
+
     it 'imageが存在していれば保存できること' do
       @message.content = nil
       expect(@message).to be_valid
-
     end
+
     it 'contentとimageが空では保存できないこと' do
       @message.content = nil
       @message.image = nil
-      expect(@message.errors.full_messages).to include("Content and")
+      @message.valid?
+      expect(@message.errors.full_messages).to include("Content can't be blank")
+    end
+
+    it 'roomが紐付いていないと保存できないこと' do
+      @message.room = nil
+      @message.valid?
+      expect(@message.errors.full_messages).to include("Room must exist")
+    end
+
+    it 'userが紐付いていないと保存できないこと' do
+      @message.user = nil
+      @message.valid?
+      expect(@message.errors.full_messages).to include("User must exist")
     end
   end
 end
